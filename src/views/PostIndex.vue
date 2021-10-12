bc9f730565ded4a585c888947474accdd1c8651c
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-12 11:35:16 -0500

    Added Vue2Filters library to filter/search posts based on user input

--- src/views/PostIndex.vue
+++ src/views/PostIndex.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="postIndex">
     <h1>All Posts</h1>
+    <div>
+      Search:
+      <input type="text" v-model="searchFilter" />
+    </div>
     <div class="row">
-      <div class="col-sm-4" v-for="post in posts" v-bind:key="post.id">
+      <div class="col-sm-4" v-for="post in filterBy(posts, searchFilter, 'title', 'description')" v-bind:key="post.id">
         <div class="card text-center">
           <div class="card-body">
             <h5 class="card-title">{{ post.title }}</h5>
@@ -35,11 +39,14 @@ h1 {
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function () {
     return {
       posts: [],
+      searchFilter: "",
     };
   },
   created: function () {
